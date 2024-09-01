f52aee710fde9a9c28221056f739c5bbe4202b7f
Walter <w.estebancr@gmail.com>
2024-09-01 15:42:37 +0200

    update feature added

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ from flask import Flask, render_template, request, redirect, url_for
 import json
 
 app = Flask(__name__)
+def fetch_post_by_id(post_id):
+    with open("blog_post.json", "r") as data_file:
+        blog_posts = json.loads(data_file.read())
+    for post in blog_posts:
+        if int(post_id) == int(post["id"]):
+            return post
 
 @app.route("/")
 def index():
@@ -36,15 +42,48 @@ def add():
 def delete(post_id):
     with open("blog_post.json", "r") as data_file:
         blog_posts = json.loads(data_file.read())
-    for blog in blog_posts:
-        if int(post_id) == int(blog['id']):
-            blog_posts.remove(blog)
+    for post in blog_posts:
+        if int(post_id) == int(post['id']):
+            blog_posts.remove(post)
 
     blog_posts_json = json.dumps(blog_posts)
     with open("blog_post.json", "w") as new_file:
         new_file.write(blog_posts_json)
 
     return redirect(url_for('index'))
+
+@app.route("/update/<int:post_id>", methods = ["GET", "POST"])
+def update(post_id):
+    # Fetch the blog posts from the JSON file
+    post = fetch_post_by_id(post_id)
+    if post is None:
+        # Post not found
+        return "Post not found", 404
+    if request.method == "POST":
+        print("here post")
+        # Update the post in the JSON file
+        # Redirect back to index
+        title_from_form = request.form.get("title")
+        author_from_form = request.form.get("author")
+        content_from_form = request.form.get("content")
+        with open("blog_post.json", "r") as data_file:
+            blog_posts = json.loads(data_file.read())
+        for post in blog_posts:
+            if int(post["id"]) == int(post_id):
+                post["title"] = title_from_form
+                post["author"] = author_from_form
+                post["content"] = content_from_form
+
+        blog_posts_json = json.dumps(blog_posts)
+        with open("blog_post.json", "w") as new_file:
+            new_file.write(blog_posts_json)
+        return redirect(url_for("index"))
+    else:
+        print("here get")
+        redirect(url_for("update", post_id=post_id))
+
+    return render_template('update.html', post=post)
+
 
 
 
